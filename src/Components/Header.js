e587fcd8711def48610fdb115edca11b79ff61c9
Jeongminseok2 <jms042799@naver.com>
2021-11-13 05:03:11 +0900

    home,header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,48 @@
 import React from "react";
-import {FaUserCircle,FaBell,FaRegSun} from 'react-icons/fa';
+import { FaUserCircle, FaBell, FaRegSun } from "react-icons/fa";
+import { GoOrganization } from "react-icons/go";
+import { GrLogout } from "react-icons/gr";
 import { Link } from "react-router-dom";
 const Header = () => {
-    return (
-        <>
-        <Link to="/board/2500">과외 학생 모집</Link>
-        <Link to="/teachers">선생님 목록</Link>
-        <FaUserCircle size="24"/>
-        <FaBell size="24"/>
-        <FaRegSun size="24"/>
-        <button>Logout</button>
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <div className="inner">
+          <a href="/" className="logo">
+            <GoOrganization size="66" color="saddlebrown" />
+          </a>
+          <div className="sub-menu">
+            <ul className="menu">
+              <li>
+                <p>
+                  <Link to="/board/2500">
+                    <button className="btn-header">과외 학생 모집</button>
+                  </Link>
+                </p>
+              </li>
+              <li>
+                <Link to="/teachers">
+                  <button className="btn-header">선생님 목록</button>
+                </Link>
+              </li>
+              <li>
+                <FaUserCircle size="30" />
+              </li>
+              <li>
+                <FaBell size="30" />
+              </li>
+              <li>
+                <FaRegSun size="30" />
+              </li>
+              <li>
+                <GrLogout size="30" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default Header;
