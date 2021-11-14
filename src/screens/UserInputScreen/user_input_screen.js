5456906af5649cd55c7a70dcde55b424455af31e
Jeongminseok2 <jms042799@naver.com>
2021-11-14 18:10:01 +0900

    css+++

--- src/screens/UserInputScreen/user_input_screen.js
+++ src/screens/UserInputScreen/user_input_screen.js
@@ -23,6 +23,7 @@ const interest = [
   "지구과학",
   "코딩",
 ];
+
 const role = ["학생", "선생님"];
 const student = ["중학생", "고등학생"];
 
@@ -56,11 +57,11 @@ function UserInputScreen() {
   const [UserCareer, setUserCareer] = useState([]);
 
   const addCareer = () => {
-    if(CareerInput==="") {
+    if (CareerInput === "") {
       alert("경력을 입력해주세요");
-    }else {
-    setUserCareer([...UserCareer, CareerInput]);
-    setCareerInput("");
+    } else {
+      setUserCareer([...UserCareer, CareerInput]);
+      setCareerInput("");
     }
   };
 
@@ -98,7 +99,7 @@ function UserInputScreen() {
         role: role[UserRole],
         student: student[UserStudent],
         career: UserCareer,
-        lectures : []
+        lectures: [],
       };
       await addDoc(collection(db, "User"), body).then((e) =>
         history.push("/main", {
@@ -109,55 +110,79 @@ function UserInputScreen() {
   };
   return (
     <div className="user-input-screen">
-      정보를 입력해주세요
-      <div className="user-input-container">
-        <input
-          required
-          name="name"
-          onChange={inputChange}
-          value={UserName}
-          placeholder="이름을 입력하세요"
-        />
-        <select name="interest" onChange={inputChange} value={UserInterest}>
-          {interest.map((item, index) => (
-            <option value={index} key={index}>
-              {item}
-            </option>
-          ))}
-        </select>
-        <select name="role" onChange={inputChange} value={UserRole}>
-          {role.map((item, index) => (
-            <option value={index} key={index}>
-              {item}
-            </option>
-          ))}
-        </select>
-        {UserRole === "0" ? (
-          <select name="student" onChange={inputChange} value={UserStudent}>
-            {student.map((item, index) => (
+      <div className="input-header">사용자 정보를 입력해주세요</div>
+      <table className="input-table">
+        <tr>
+          <th>성함</th>
+          <th>과목</th>
+          <th>교사/학생</th>
+        </tr>
+        <tr></tr>
+        <td>
+          <input
+            required
+            name="name"
+            onChange={inputChange}
+            value={UserName}
+            placeholder="이름을 입력하세요"
+          />
+        </td>
+        <td>
+          <select name="interest" onChange={inputChange} value={UserInterest}>
+            {interest.map((item, index) => (
               <option value={index} key={index}>
                 {item}
               </option>
             ))}
           </select>
-        ) : (
-          <div>
-            <input
-              name="careerInput"
-              onChange={inputChange}
-              value={CareerInput}
-              placeholder="경력을 입력하세요"
-            />
-            <button onClick={addCareer}>경력추가</button>
-          </div>
-        )}
-        {UserCareer.map((e) => {
-          return <div>{e}</div>;
-        })}
-        <button type="submit" onClick={submitToMainScreen}>
-          정보입력완료
-        </button>
-      </div>
+        </td>
+        <td>
+          <select name="role" onChange={inputChange} value={UserRole}>
+            {role.map((item, index) => (
+              <option value={index} key={index}>
+                {item}
+              </option>
+            ))}
+          </select>
+          {UserRole === "0" ? (
+            <select name="student" onChange={inputChange} value={UserStudent}>
+              {student.map((item, index) => (
+                <option value={index} key={index}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <div>
+              <input
+                name="careerInput"
+                onChange={inputChange}
+                value={CareerInput}
+                placeholder="경력을 입력하세요"
+              />
+
+              <button className="btn-add-carrer" onClick={addCareer}>
+                경력추가
+              </button>
+            </div>
+          )}
+          {/* 여기 리스트화 */}
+          {UserCareer.map((e) => {
+            return (
+              <ul className="carrer-list">
+                <li>{e}</li>
+              </ul>
+            );
+          })}
+        </td>
+      </table>
+      <button
+        className="btn-complete-input"
+        type="submit"
+        onClick={submitToMainScreen}
+      >
+        정보입력완료
+      </button>
     </div>
   );
 }
